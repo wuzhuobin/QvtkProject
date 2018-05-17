478067b010b3ac202f635b0a367796e8cfba3eba
wuzhuobin <jiejin2022@163.com>
2018-05-17 18:23:27 +0800

    update QvtkUtil, NeuralTube with property.

--- Src/Data/QvtkNeuralTube.cpp
+++ Src/Data/QvtkNeuralTube.cpp
@@ -9,81 +9,108 @@
 namespace Q {
 	namespace vtk {
 
-Q_VTK_DATA_CPP(NeuralTube);
-NeuralTube::NeuralTube()
-	:m_intermediatePoint{0,0,0}
-{
-	this->m_lineSource = vtkLineSource::New();
-	this->m_lineSource->SetResolution(1);
-	this->m_tubeFilter = vtkTubeFilter::New();
-	this->m_tubeFilter->SetInputConnection(this->m_lineSource->GetOutputPort());
-	this->m_tubeFilter->SetCapping(true);
-	this->m_tubeFilter->SetRadius(0);
-}
+		Q_VTK_DATA_CPP(NeuralTube);
+		NeuralTube::NeuralTube()
+			:m_intermediatePoint{ 0,0,0 }
+		{
+			this->m_lineSource = vtkLineSource::New();
+			this->m_lineSource->SetResolution(1);
+			this->m_tubeFilter = vtkTubeFilter::New();
+			this->m_tubeFilter->SetInputConnection(this->m_lineSource->GetOutputPort());
+			this->m_tubeFilter->SetCapping(true);
+			this->m_tubeFilter->SetRadius(0);
+		}
 
-NeuralTube::~NeuralTube()
-{
-	this->m_lineSource->Delete();
-	this->m_tubeFilter->Delete();
-}
+		NeuralTube::~NeuralTube()
+		{
+			this->m_lineSource->Delete();
+			this->m_tubeFilter->Delete();
+		}
 
-void NeuralTube::readXML(const QDomElement & xml, QString directoryPath)
-{
-	PolyData::readXML(xml, directoryPath);
-	this->setTubeRadius(xml.attribute(K.TubeRadius).toDouble());
-	this->setTubeLength(xml.attribute(K.TubeLength).toDouble());
-	NeuralTube::readArray(K.IntermediatePoint, xml, this->m_intermediatePoint, 3);
-	NeuralTube::readArray(K.EndPoint, xml, this->m_lineSource->GetPoint2(), 3);
-}
+		void NeuralTube::readXML(const QDomElement & xml, QString directoryPath)
+		{
+			PolyData::readXML(xml, directoryPath);
+			this->setTubeRadius(xml.attribute(K.TubeRadius).toDouble());
+			this->setTubeLength(xml.attribute(K.TubeLength).toDouble());
+			NeuralTube::readArray(K.IntermediatePoint, xml, this->m_intermediatePoint, 3);
+			NeuralTube::readArray(K.EndPoint, xml, this->m_lineSource->GetPoint2(), 3);
+		}
 
-void NeuralTube::writeXML(QDomElement & xml, QString directoryPath) const
-{
-	PolyData::writeXML(xml, directoryPath);
-	xml.setAttribute(K.TubeRadius, this->getTubeRadius());
-	xml.setAttribute(K.TubeLength, this->getTubeLength());
-	NeuralTube::writeArray(K.IntermediatePoint, xml, this->getIntermediatePoint(), 3);
-	NeuralTube::writeArray(K.EndPoint, xml, this->getEndPoint(), 3);
-}
+		void NeuralTube::writeXML(QDomElement & xml, QString directoryPath) const
+		{
+			PolyData::writeXML(xml, directoryPath);
+			xml.setAttribute(K.TubeRadius, this->getTubeRadius());
+			xml.setAttribute(K.TubeLength, this->getTubeLength());
+			NeuralTube::writeArray(K.IntermediatePoint, xml, this->getIntermediatePoint(), 3);
+			NeuralTube::writeArray(K.EndPoint, xml, this->getEndPoint(), 3);
+		}
 
-bool NeuralTube::readData(QString rootDirectory)
-{
-	if (rootDirectory.isEmpty()) {
-		return PolyData::readData(rootDirectory);
-	}
-	else {
-		double vector[3];
-		vtkMath::Subtract(this->getIntermediatePoint(), this->getEndPoint(), vector);
-		vtkMath::Normalize(vector);
-		vtkMath::MultiplyScalar(vector, this->getTubeLength());
-		vtkMath::Add(this->getEndPoint(), vector, vector);
-		this->m_lineSource->SetPoint1(vector);
-		this->m_lineSource->Update();
-		int sides = this->getTubeRadius() / 1 + 10;
-		this->m_tubeFilter->SetNumberOfSides(sides);
-		this->m_tubeFilter->Update();
-		this->getPolyData()->ShallowCopy(this->m_tubeFilter->GetOutput());
-		return true;
-	}
-}
+		bool NeuralTube::readData(QString rootDirectory)
+		{
+			if (rootDirectory.isEmpty()) {
+				return PolyData::readData(rootDirectory);
+			}
+			else {
+				double vector[3];
+				vtkMath::Subtract(static_cast<const NeuralTube*>(this)->getIntermediatePoint(), static_cast<const NeuralTube*>(this)->getEndPoint(), vector);
+				vtkMath::Normalize(vector);
+				vtkMath::MultiplyScalar(vector, this->getTubeLength());
+				vtkMath::Add(static_cast<const NeuralTube*>(this)->getEndPoint(), vector, vector);
+				this->m_lineSource->SetPoint1(vector);
+				this->m_lineSource->Update();
+				int sides = this->getTubeRadius() / 1 + 10;
+				this->m_tubeFilter->SetNumberOfSides(sides);
+				this->m_tubeFilter->Update();
+				this->getPolyData()->ShallowCopy(this->m_tubeFilter->GetOutput());
+				return true;
+			}
+		}
 
-double NeuralTube::getTubeRadius() const
-{
-	return this->m_tubeFilter->GetRadius();
-}
+		double NeuralTube::getTubeRadius() const
+		{
+			return this->m_tubeFilter->GetRadius();
+		}
 
-const double * NeuralTube::getEndPoint() const
-{
-	return this->m_lineSource->GetPoint2();
-}
+		const double * NeuralTube::getEndPoint() const
+		{
+			return this->m_lineSource->GetPoint2();
+		}
 
-void NeuralTube::setTubeRadius(double radius)
-{
-	this->m_tubeFilter->SetRadius(radius);
-}
+		QList<double> NeuralTube::getEndPoint()
+		{
+			return QList<double>{this->getEndPoint()[0], this->getEndPoint()[1], this->getEndPoint()[2]};
+		}
 
-void NeuralTube::setEndPoint(double x, double y, double z)
-{
-	this->m_lineSource->SetPoint2(x, y, z);
-}
+		void NeuralTube::setTubeLength(double length)
+		{
+			this->m_tubeLength = length;
+			this->readData("...");
+			emit this->tubeLengthChanged(length);
+		}
+
+		void NeuralTube::setTubeRadius(double radius)
+		{
+			this->m_tubeFilter->SetRadius(radius);
+			this->readData("...");
+			emit this->tubeRadiusChanged(radius);
+		}
+
+		void NeuralTube::setIntermediatePoint(double x, double y, double z)
+		{
+			this->m_intermediatePoint[0] = x;
+			this->m_intermediatePoint[1] = y;
+			this->m_intermediatePoint[2] = z;
+			this->readData("...");
+			emit this->intermediatePointChanged(this->getIntermediatePoint());
+			emit this->intermediatePointChanged(this->m_intermediatePoint);
+		}
+
+		void NeuralTube::setEndPoint(double x, double y, double z)
+		{
+			this->m_lineSource->SetPoint2(x, y, z);
+			this->readData("...");
+			emit this->endPointChanged(this->getEndPoint());
+			emit this->endPointChanged(static_cast<const NeuralTube*>(this)->getEndPoint());
+		}
 	}
 }
--- Src/Data/QvtkNeuralTube.h
+++ Src/Data/QvtkNeuralTube.h
@@ -37,6 +37,26 @@ namespace Q {
 
 class NeuralTube : public PolyData {
 	Q_OBJECT;
+	Q_PROPERTY(
+		double	tubeRadius
+		READ	getTubeRadius
+		WRITE	setTubeRadius
+		NOTIFY	tubeRadiusChanged);
+	Q_PROPERTY(
+		double	tubeLength
+		READ	getTubeLength
+		WRITE	setTubeLength
+		NOTIFY	tubeLengthChanged);
+	Q_PROPERTY(
+		QList<double>	intermediatePoint
+		READ			getIntermediatePoint
+		WRITE			setIntermediatePoint
+		NOTIFY			intermediatePointChanged);
+	Q_PROPERTY(
+		QList<double>	endPoint
+		READ			getEndPoint
+		WRITE			setEndPoint
+		NOTIFY			endPointChanged);
 	Q_VTK_DATA_H(
 		NeuralTube,
 		Q_VTK_KEY(TubeRadius)
@@ -50,23 +70,28 @@ public:
 	virtual void readXML(const QDomElement &xml, QString directoryPath = QString()) override;
 	virtual void writeXML(QDomElement &xml, QString directoryPath) const override;
 	virtual bool readData(QString rootDirectory) override;
-	double getTubeRadius() const;
-	double getTubeLength() const { return this->m_tubeLength; }
-	const double* getIntermediatePoint() const { return this->m_intermediatePoint; }
-	const double* getEndPoint() const;
+	virtual double getTubeRadius() const;
+	virtual double getTubeLength() const { return this->m_tubeLength; }
+	virtual const double* getIntermediatePoint() const { return this->m_intermediatePoint; }
+	virtual QList<double> getIntermediatePoint() { return QList<double>{this->m_intermediatePoint[0], this->m_intermediatePoint[1], this->m_intermediatePoint[2]}; }
+	virtual const double* getEndPoint() const;
+	virtual QList<double> getEndPoint();
 public Q_SLOTS:
-	void setTubeRadius(double radius); 
-	void setTubeLength(double length) { 
-		this->m_tubeLength = length; 
-		this->readData("..."); }
-	void setIntermediatePoint(double x, double y, double z) {
-		this->m_intermediatePoint[0] = x;
-		this->m_intermediatePoint[1] = y;
-		this->m_intermediatePoint[2] = z;
-		this->readData("..."); }
-	void setIntermediatePoint(const double point[3]) { this->setIntermediatePoint(point[0], point[1], point[2]); }
-	void setEndPoint(double x, double y, double z);
-	void setEndPoint(const double point[3]) { this->setEndPoint(point[0], point[1], point[2]); }
+	virtual void setTubeLength(double length);
+	virtual void setTubeRadius(double radius); 
+	virtual void setIntermediatePoint(double x, double y, double z);
+	virtual void setIntermediatePoint(const double point[3]) { this->setIntermediatePoint(point[0], point[1], point[2]); }
+	virtual void setIntermediatePoint(QList<double> point) { this->setIntermediatePoint(point[0], point[1], point[2]); }
+	virtual void setEndPoint(double x, double y, double z);
+	virtual void setEndPoint(const double point[3]) { this->setEndPoint(point[0], point[1], point[2]); }
+	virtual void setEndPoint(QList<double> point) { this->setEndPoint(point[0], point[1], point[2]); }
+Q_SIGNALS:
+	void tubeRadiusChanged(double radius) const;
+	void tubeLengthChanged(double length) const;
+	void intermediatePointChanged(const double *point) const;
+	void intermediatePointChanged(QList<double> point) const;
+	void endPointChanged(const double *point) const;
+	void endPointChanged(QList<double> point) const;
 protected:
 	virtual Data* newInstance() const override { return new NeuralTube; }
 	vtkLineSource *m_lineSource;
