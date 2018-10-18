8022f1149fb002c9861e1bf1d86a2f1d3b44cfe6
wuzhuobin <jiejin2022@163.com>
2018-10-18 18:38:00 +0800

    Using vtkBrainExtractionFIlter instread of vtkBET2Filter. 1. vtkBET2Filter would have dependency, which hard to build this project. 2. vtkBrainExtraction is the vtk implementation, good compatiable to this project.

---
+++ Src/Filter/cmake/SubDirectory.cmake
@@ -0,0 +1,22 @@
+# @file	    SubDirectory.cmake
+# @brief	CMake script for adding sub modules.
+# @author	WUZHUOBIN
+# @version	1.0
+# @since	Aug.23.2018
+# @date 	Aug.23.2018
+# @log
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#			Copyright (c) WUZHUOBIN 											    #
+#			All rights reserved.												    #
+#			See Copyright.txt													    #
+#			This software is distributed WITHOUT ANY WARRANTY; without even		    #
+#			the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR	    #
+#			PURPOSE.  See the above copyright notice for more information.		    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# e.g.
+# add_subdirectory(QvtkUtil)
+# add_subdirectory(Filter)
+# add_subdirectory(Widget)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # ADD BELOW # # # # # # # # # # # # # # # # # # # # 
+add_subdirectory(vtkBrainExtractionFilter)
